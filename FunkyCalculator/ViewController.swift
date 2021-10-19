1f0d9fa3c36734f4fe78fb235e5e8d98a11f31be
Utsav Barnwal <u.barnwal36@gmail.com>
2021-10-20 02:33:40 +0530

    Initial Commit

---
+++ FunkyCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FunkyCalculator
+//
+//  Created by Utsav Barnwal on 20/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
